a951576d6f19c74e25cf1e2763ac8eeb8d1fbefa
Luis Romero <luisromero@Luiss-MacBook-Pro.local>
2019-04-02 19:23:48 -0600

    Eventos en botones terminado.

--- app/src/main/java/com/example/app_2051_01/MainActivity.java
+++ app/src/main/java/com/example/app_2051_01/MainActivity.java
@@ -8,13 +8,39 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button BtnSimple;
+    public ImageButton BtnImage;
+    public Button BtnSimpleConImagen;
+    public TextView LblMensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtnSimple = (Button) findViewById(R.id.BtnSimple);
+        BtnImage = (ImageButton) findViewById(R.id.BtnImage);
+        BtnSimpleConImagen = (Button) findViewById(R.id.BtnSimpleConImagen);
+        LblMensaje = (TextView)findViewById(R.id.LblMensaje);
+
+        View.OnClickListener Escuchador = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Acción a realizar
+                LblMensaje.setText("HOLA, acaban de dar clic en el botón!" + v.getId());
+            }
+        };
+
+        BtnSimple.setOnClickListener(Escuchador);
+        BtnImage.setOnClickListener(Escuchador);
+        BtnSimpleConImagen.setOnClickListener(Escuchador);
+
     }
 
 }
